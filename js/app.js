f17874cf5d7747cb0fb4a3fd54305489e1f4c510
MauricioAcosta <andres.taamap@gmail.com>
2016-10-11 23:10:06 -0500

    Improved order

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 // Declare app level module which depends on filters, and services
-angular.module('SLUD', ['ngResource', 'ngRoute' ])
-
-.config(['$routeProvider', function ($routeProvider) {
-  console.log("Entro");
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/home/home.html',
-      controller: 'HomeController'})
-    .otherwise({redirectTo: '/'});
-    }]);
+angular.module('SLUD', ['ngResource', 'ngRoute']).config([
+    '$routeProvider',
+    function($routeProvider) {
+        console.log("Entro");
+        $routeProvider.when('/', {
+            templateUrl: 'views/home/home.html',
+            controller: 'HomeController'
+        }).otherwise({redirectTo: '/'});
+    }
+]);
--- js/home/home-controller.js
+++ js/home/home-controller.js
@@ -1,15 +1,16 @@
-angular.module('SLUD')
-  .controller('HomeController', ['$http','$scope', function ($http,$scope) {
-    console.log("Entro");
-    var f = [];
-    $http.get("http://slud.pythonanywhere.com/api/charlas")
-      .success(function(data) {
-        // data.forEach(function(entry, index) {
-          // f[index] = entry.Id;
-        // });
-        console.log(data);
-        // $scope.PropietarioIds = f;
-      })
-      .error(function(err) {});
+angular.module('SLUD').controller('HomeController', [
+    '$http',
+    '$scope',
+    function($http, $scope) {
+        console.log("Entro");
+        var f = [];
+        $http.get("http://slud.pythonanywhere.com/api/charlas").success(function(data) {
+            // data.forEach(function(entry, index) {
+            // f[index] = entry.Id;
+            // });
+            console.log(data);
+            // $scope.PropietarioIds = f;
+        }).error(function(err) {});
 
-  }]);
+    }
+]);
