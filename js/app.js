f568ed7605b1d31acecfb2408bae265eb0ae7630
David Castelblanco B <david.castelblanco@hotmail.com>
2016-10-12 13:21:30 -0500

    Se agregan fotos de algunos expositores, y se consume la información de los expositores desde el API

--- js/app.js
+++ js/app.js
@@ -12,24 +12,10 @@
   })
   .controller('Expositores', ['$http' , function Expositores($http){
     // Obtener los expositores del API
-    // $http.get('https://slud.pythonanywhere.com/api/speakers/')
-    //   .success(data => {
-    //     this.lista = data;
-    //   })
-
-    // Expositores de prueba
-    this.lista = [
-      {
-        'foto': 'http://www.oalaska.com/wp-content/themes/pointfinder/images/empty_avatar.jpg',
-        'nombre': 'Prueba 1',
-        'trabajo': 'Grupo GNU/Linux'
-      },
-      {
-        'foto': 'http://www.oalaska.com/wp-content/themes/pointfinder/images/empty_avatar.jpg',
-        'nombre': 'Prueba 2',
-        'trabajo': 'Grupo GNU/Linux'
-      }
-    ];
+    $http.get('https://slud.pythonanywhere.com/api/speakers/')
+      .success(data => {
+        this.lista = data;
+      })
   }])
   .controller('Agenda', ['$http', function Agenda($http){
     // Consultar las charlas del API
